86d81b45c085c191acd2117ae97b3c095a64833d
KaranLalwani-dev <karanlalwani2068k@gmail.com>
2026-05-25 16:47:56 +0530

    Added ressiliance 4j, tried retry, circuit breaker, rate limiter on the create method of order service

--- order-service/src/main/java/com/karandev/ecommerce/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/karandev/ecommerce/order_service/service/OrdersService.java
@@ -6,6 +6,7 @@ import com.karandev.ecommerce.order_service.entity.OrderItem;
 import com.karandev.ecommerce.order_service.entity.OrderStatus;
 import com.karandev.ecommerce.order_service.entity.Orders;
 import com.karandev.ecommerce.order_service.repository.OrdersRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -33,8 +34,11 @@ public class OrdersService {
         Orders order = orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found"));
         return modelMapper.map(order, OrderRequestDto.class);
     }
-
+    //    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker", fallbackMethod = "createOrderFallback")
+//    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
+        log.info("Calling the createOrder method");
         Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
 
         Orders order = modelMapper.map(orderRequestDto, Orders.class);
@@ -47,5 +51,11 @@ public class OrdersService {
         Orders savedOrder = orderRepository.save(order);
 
         return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable) {
+        log.error("Fallback occurred due to : {}", throwable.getMessage());
+
+        return new OrderRequestDto();
     }
 }
